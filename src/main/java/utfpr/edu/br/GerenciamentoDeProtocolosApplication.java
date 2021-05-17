9ca872f432a8b0495c1ac3998a7d87411f46559c
HeloisaSoaresUtpr <helo_soarescosta@hotmail.com>
2021-05-16 22:03:45 -0300

    commit inicial

---
+++ src/main/java/utfpr/edu/br/GerenciamentoDeProtocolosApplication.java
@@ -0,0 +1,13 @@
+package utfpr.edu.br;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciamentoDeProtocolosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GerenciamentoDeProtocolosApplication.class, args);
+	}
+
+}
---
+++ src/test/java/utfpr/edu/br/GerenciamentoDeProtocolosApplicationTests.java
@@ -0,0 +1,13 @@
+package utfpr.edu.br;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciamentoDeProtocolosApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
